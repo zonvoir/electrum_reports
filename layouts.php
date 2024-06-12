3d4c3c6d1f6f03e72605df98dfd327c72ab903de
zonvoir <zonvoir@gmail.com>
2024-06-12 12:07:29 +0530

    Check-user-login-session-and-role-for-every-page

--- layouts.php
+++ layouts.php
@@ -3,116 +3,111 @@ require 'header.php';
 ?>
 
 <body class="wrap-tbl-content">
-<?php
-require 'navigation.php';
-?>
-<div class="container" style="padding-top: 50px;padding-bottom: 20px;">
-
-<div class="card border-0 tbl-csmz mb-5">
+    <?php
+    require 'navigation.php';
+    ?>
+    <div class="container" style="padding-top: 50px;padding-bottom: 20px;">
+        <div class="card border-0 tbl-csmz mb-5">
+            <div class="card-header p-3 ">
+                <h4>Layouts</h4>
+            </div>
+            <div class="card-body p-0">
+                <table id="layoutsTable" class="display table table-sm  table-hover" style="width:100%">
+                    <thead class="table-light">
+                        <tr>
+                            <th>ID</th>
+                            <th>Template Name</th>
+                            <th>Layout Name</th>
+                            <th style="width: 80px;">Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody></tbody>
+                </table>
+            </div>
+        </div>
+    </div>
     
-    <div class="card-header p-3 ">
-    <h4>Layouts</h4>
-     </div>
+    <?php
+    require 'modals.php';
+    ?>
 
-     <div class="card-body p-0">
-    <table id="layoutsTable" class="display table table-sm  table-hover" style="width:100%">
-        <thead class="table-light">
-            <tr>
-                <th>ID</th>
-                <th>Template Name</th>
-                <th>Layout Name</th>
-                <th style="width: 80px;">Actions</th>
-            </tr>
-        </thead>
-        <tbody></tbody>
-    </table>
-    </div>
-    </div>
-
-</div>
-
-<?php
-require 'modals.php';
-?>
-</body>
-
-<script>
-$(document).ready(function() {
-    $('#layoutsTable').DataTable({
-        "draw": 1,
-        "processing": true,
-        "serverSide": true,
-        "recordsTotal": 5, // Total number of records in your dataset
-        "recordsFiltered": 5, // Total number of records after filtering (if you're implementing filtering)
-        "ajax": {
-            "url": "functions/add-title.php", // Path to your server-side script
-            "type": "POST",
-            data: {
-                action: "allLayouts"
+    <script>
+    $(document).ready(function() {
+        $('#layoutsTable').DataTable({
+            "draw": 1,
+            "processing": true,
+            "serverSide": true,
+            "recordsTotal": 5, // Total number of records in your dataset
+            "recordsFiltered": 5, // Total number of records after filtering (if you're implementing filtering)
+            "ajax": {
+                "url": "functions/add-title.php", // Path to your server-side script
+                "type": "POST",
+                data: {
+                    action: "allLayouts"
+                },
             },
-        },
-        "columns": [
-            {
-                "data": "id"
-            },
-            {
-                "data": "template_name"
-            },
-            {
-                "data": "layout_name"
-            },
-            {
-                "data": null,
-                "defaultContent": "<div class='d-flex'>\
-                                    <button class='btn btn-sm edit-btn me-2'>\<i class='fa-solid fa-pen-to-square '></i></button>\
-                                    <button class='btn btn-sm delete-btn'><i class='fa-solid fa-trash'></i></button>\
-                                    </div>",
-                "orderable": false
-            },
-        ],
-        "pagingType": "full_numbers",
-    });
+            "columns": [
+                {
+                    "data": "id"
+                },
+                {
+                    "data": "template_name"
+                },
+                {
+                    "data": "layout_name"
+                },
+                {
+                    "data": null,
+                    "defaultContent": "<div class='d-flex'>\
+                                        <button class='btn btn-sm edit-btn me-2'>\<i class='fa-solid fa-pen-to-square '></i></button>\
+                                        <button class='btn btn-sm delete-btn'><i class='fa-solid fa-trash'></i></button>\
+                                        </div>",
+                    "orderable": false
+                },
+            ],
+            "pagingType": "full_numbers",
+        });
 
-    $(document).on('click', '.edit-btn', function() 
-    {
-        var row = $(this).closest('tr');
-        var data = $('#layoutsTable').DataTable().row(row).data();
-        var layoutId = data.id;
-        window.location.href = 'edit-layout.php?id=' + layoutId;
-    });
-
-    $(document).on('click', '.delete-btn', function() 
-    {
-        if (confirm("Are you sure you want to delete this record?")) 
+        $(document).on('click', '.edit-btn', function() 
         {
             var row = $(this).closest('tr');
             var data = $('#layoutsTable').DataTable().row(row).data();
-            var layout_id = data.id;
-            deleteLayout(layout_id, row);
-        }
-    });
-    function deleteLayout(layout_id, row) 
-    {
-        $.ajax({
-            type: 'POST',
-            url: 'functions/add-title.php',
-            data: {
-                action: 'removeLayout',
-                layout_id: layout_id
-            },
-            success: function(data) {
-                var response = JSON.parse(data)
-                toastr.success(response.message, 'Success!')
-                $('#layoutsTable').DataTable().row(row).remove().draw();
-            },
-            error: function() {
-                // Handle error case
-                console.log('Error loading layouts.');
+            var layoutId = data.id;
+            window.location.href = 'edit-layout.php?id=' + layoutId;
+        });
+
+        $(document).on('click', '.delete-btn', function() 
+        {
+            if (confirm("Are you sure you want to delete this record?")) 
+            {
+                var row = $(this).closest('tr');
+                var data = $('#layoutsTable').DataTable().row(row).data();
+                var layout_id = data.id;
+                deleteLayout(layout_id, row);
             }
         });
-    }
-});
-</script>
+        function deleteLayout(layout_id, row) 
+        {
+            $.ajax({
+                type: 'POST',
+                url: 'functions/add-title.php',
+                data: {
+                    action: 'removeLayout',
+                    layout_id: layout_id
+                },
+                success: function(data) {
+                    var response = JSON.parse(data)
+                    toastr.success(response.message, 'Success!')
+                    $('#layoutsTable').DataTable().row(row).remove().draw();
+                },
+                error: function() {
+                    // Handle error case
+                    console.log('Error loading layouts.');
+                }
+            });
+        }
+    });
+    </script>
 <?php
 require 'footer.php';
 ?>
--- templates.php
+++ templates.php
@@ -3,145 +3,139 @@ require 'header.php';
 ?>
 
 <body class="wrap-tbl-content">
-<?php
-require 'navigation.php';
-?>
-
-<div class="container" style="padding-top: 50px;">
-    <div class="card border-0 tbl-csmz mb-5">
-     <div class="card-header p-3 ">
-     <h4>Templates</h4>
-     </div>
-
-     <div class="card-body p-0">
-    <table id="templatesTable" class="display table table-sm  table-hover" style="width:100%">
-        <thead>
-            <tr>
-                <th>ID</th>
-                <th>Template Name</th>
-                <th style="width: 80px;">Actions</th>
-            </tr>
-        </thead>
-        <tbody></tbody>
-    </table>
+    <?php
+    require 'navigation.php';
+    ?>
+    <div class="container" style="padding-top: 50px;">
+        <div class="card border-0 tbl-csmz mb-5">
+            <div class="card-header p-3 ">
+                <h4>Templates</h4>
+            </div>
+            <div class="card-body p-0">
+                <table id="templatesTable" class="display table table-sm table-hover" style="width:100%">
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Template Name</th>
+                            <th style="width: 80px;">Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody></tbody>
+                </table>
+            </div>
+        </div>
     </div>
 
-    </div>
-</div>
+    <?php
+    require 'modals.php';
+    ?>
 
-
-<?php
-require 'modals.php';
-?>
-</body>
-
-<script>
-$(document).ready(function() {
-    $('#templatesTable').DataTable({
-        "draw": 1,
-        "processing": true,
-        "serverSide": true,
-        "recordsTotal": 5, // Total number of records in your dataset
-        "recordsFiltered": 5, // Total number of records after filtering (if you're implementing filtering)
-        "ajax": {
-            "url": "functions/add-title.php", // Path to your server-side script
-            "type": "POST",
-            data: {
-                action: "allTemplates"
+    <script>
+    $(document).ready(function() {
+        $('#templatesTable').DataTable({
+            "draw": 1,
+            "processing": true,
+            "serverSide": true,
+            "recordsTotal": 5, // Total number of records in your dataset
+            "recordsFiltered": 5, // Total number of records after filtering (if you're implementing filtering)
+            "ajax": {
+                "url": "functions/add-title.php", // Path to your server-side script
+                "type": "POST",
+                data: {
+                    action: "allTemplates"
+                },
             },
-        },
-        "columns": [
-            {
-                "data": "id"
-            },
-            // {
-            //     "data": "template_name",
-            //     "render": function(data, type, row) {
-            //         var templateName = data || "";
-            //         return '<button class="btn btn-link template-btn" data-template-id="' + row.id + '">' + templateName + '</button>';
-            //     }
-            // },
-            {
-                "data": "template_name",
-                "render": function(data, type, row) {
-                    var templateName = data || "";
-                    return '<a href="view-template.php?id='+row.id+'">' + templateName + '</a>';
+            "columns": [
+                {
+                    "data": "id"
+                },
+                // {
+                //     "data": "template_name",
+                //     "render": function(data, type, row) {
+                //         var templateName = data || "";
+                //         return '<button class="btn btn-link template-btn" data-template-id="' + row.id + '">' + templateName + '</button>';
+                //     }
+                // },
+                {
+                    "data": "template_name",
+                    "render": function(data, type, row) {
+                        var templateName = data || "";
+                        return '<a href="view-template.php?id='+row.id+'">' + templateName + '</a>';
+                    }
+                },
+                {
+                    "data": null,
+                    "defaultContent": "<div class='d-flex'>\
+                                        <button class='btn  btn-sm edit-btn me-2'>\<i class='fa-solid fa-pen-to-square '></i></button>\
+                                        <button class='btn  btn-sm delete-btn me-2'><i class='fa-solid fa-trash'></i></button>\
+                                        <button class='btn  btn-sm analysis-btn'><i class='fa-solid fa-chart-line'></i></button>\
+                                        </div>",
+                    "orderable": false
                 }
-            },
-            {
+            ],
+            "pagingType": "full_numbers",
+        });
 
-                "data": null,
-                "defaultContent": "<div class='d-flex'>\
-                                    <button class='btn  btn-sm edit-btn me-2'>\<i class='fa-solid fa-pen-to-square '></i></button>\
-                                    <button class='btn  btn-sm delete-btn me-2'><i class='fa-solid fa-trash'></i></button>\
-                                    <button class='btn  btn-sm analysis-btn'><i class='fa-solid fa-chart-line'></i></button>\
-                                    </div>",
-                "orderable": false
-            }
-        ],
-        "pagingType": "full_numbers",
-    });
+        // $('#templatesTable').on('click', 'tbody tr', function() 
+        // {
+        //     if (!$(event.target).closest('.delete-btn, .edit-btn').length) {
+        //         var data = $('#templatesTable').DataTable().row(this).data();
+        //         if (data && data.id) {
+        //             window.location.href = 'view-template.php?id=' + data.id;
+        //         }
+        //     }
+        // });
 
-    // $('#templatesTable').on('click', 'tbody tr', function() 
-    // {
-    //     if (!$(event.target).closest('.delete-btn, .edit-btn').length) {
-    //         var data = $('#templatesTable').DataTable().row(this).data();
-    //         if (data && data.id) {
-    //             window.location.href = 'view-template.php?id=' + data.id;
-    //         }
-    //     }
-    // });
-
-    $(document).on('click', '.edit-btn', function() 
-    {
-        var row = $(this).closest('tr');
-        var data = $('#templatesTable').DataTable().row(row).data();
-        $('#templateName').val(data.template_name);
-        $('#template_id').val(data.id);
-        $("#templateModalLabel").text("Edit Template");
-        $('#templateModal').modal('show');
-    });
-
-    $(document).on('click', '.delete-btn', function() 
-    {
-        if (confirm("Are you sure you want to delete this record?")) 
+        $(document).on('click', '.edit-btn', function() 
         {
             var row = $(this).closest('tr');
             var data = $('#templatesTable').DataTable().row(row).data();
-            var template_id = data.id;
-            deleteTemplate(template_id, row);
-        }
-    });
-    function deleteTemplate(template_id, row) 
-    {
-        $.ajax({
-            type: 'POST',
-            url: 'functions/add-title.php',
-            data: {
-                action: 'removeTemplate',
-                template_id: template_id
-            },
-            success: function(data) {
-                var response = JSON.parse(data)
-                toastr.success(response.message, 'Success!')
-                $('#templatesTable').DataTable().row(row).remove().draw();
-            },
-            error: function() {
-                console.log('Error loading layouts.');
+            $('#templateName').val(data.template_name);
+            $('#template_id').val(data.id);
+            $("#templateModalLabel").text("Edit Template");
+            $('#templateModal').modal('show');
+        });
+
+        $(document).on('click', '.delete-btn', function() 
+        {
+            if (confirm("Are you sure you want to delete this record?")) 
+            {
+                var row = $(this).closest('tr');
+                var data = $('#templatesTable').DataTable().row(row).data();
+                var template_id = data.id;
+                deleteTemplate(template_id, row);
             }
         });
-    }
-
-    $(document).on('click', '.analysis-btn', function() 
-    {
-        var row = $(this).closest('tr');
-        var data = $('#templatesTable').DataTable().row(row).data();
-        if (data && data.id) {
-            window.location.href = 'analysis.php?id=' + data.id;
+        function deleteTemplate(template_id, row) 
+        {
+            $.ajax({
+                type: 'POST',
+                url: 'functions/add-title.php',
+                data: {
+                    action: 'removeTemplate',
+                    template_id: template_id
+                },
+                success: function(data) {
+                    var response = JSON.parse(data)
+                    toastr.success(response.message, 'Success!')
+                    $('#templatesTable').DataTable().row(row).remove().draw();
+                },
+                error: function() {
+                    console.log('Error loading layouts.');
+                }
+            });
         }
+
+        $(document).on('click', '.analysis-btn', function() 
+        {
+            var row = $(this).closest('tr');
+            var data = $('#templatesTable').DataTable().row(row).data();
+            if (data && data.id) {
+                window.location.href = 'analysis.php?id=' + data.id;
+            }
+        });
     });
-});
-</script>
+    </script>
 <?php
 require 'footer.php';
 ?>
